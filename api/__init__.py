04b9cd431aa9835f203125065efa00349b98248f
HieuNTg <hiunguyen2207@gmail.com>
2026-04-06 11:52:01 +0700

    refactor: remove Codex page (routes, frontend, nav entry)

--- api/__init__.py
+++ api/__init__.py
@@ -26,7 +26,6 @@ from api.usage_routes import router as usage_router
 from api.eval_routes import router as eval_router
 from api.share_routes import router as share_router
 from api.prompt_routes import router as prompt_router
-from api.codex_routes import router as codex_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(auth_router)
@@ -44,7 +43,6 @@ api_router.include_router(usage_router)
 api_router.include_router(eval_router)
 api_router.include_router(share_router)
 api_router.include_router(prompt_router)
-api_router.include_router(codex_router)
 
 _log = logging.getLogger(__name__)
 
